0f52328dee788417a23e1bc0b8da804c881a65b4
Egor1dzeN <egormorozov123450@gmail.com>
2025-02-10 20:40:05 +0300

    minor fix

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,13 @@
 // Copyright 2021 NNTU-CS
 #include <cmath>
+
 int gcd(int a, int b) {
-    a = abs(a);
-    b = abs(b);
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
+  a = abs(a);
+  b = abs(b);
+  while (b != 0) {
+    int temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
 }
